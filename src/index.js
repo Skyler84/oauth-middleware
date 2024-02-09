0d30211c3fe233870dd81fc96c3d9ec2ee79dfcb
Skyler Mansfield <90399509+Skyler84@users.noreply.github.com>
2024-02-09 13:45:10 +0000

    Mostly setup Added gcloud app config for deploy Added CORS middleware

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import Express from 'express'
+import * as cors from 'cors'
 
-const HOST="0.0.0.0"
-const PORT=8000
+
+const HOST=process.env.HOST || "0.0.0.0"
+const PORT=process.env.PORT || 8000
 
 let app = Express()
+app.use(cors())
 
 let authorizations = {};
 
@@ -11,16 +14,17 @@ app.get('/authorized', (req, res) => {
     let code = req.query['code'];
     let state = req.query['state'];
     authorizations[state] = code;
+    res.sendStatus(200);
 })
 
 app.get('/authorization', (req, res) => {
     let state = req.query['state'];
     if (!state in authorizations){
-        res.status(404);
+        res.sendStatus(404);
         return;
     }
     res.send({code: authorizations[state]});
-    authorizations.remove(state);
+    authorizations.delete(state);
 })
 
 app.listen(PORT, HOST, ()=>{console.log(`Listening on ${HOST}:${PORT}`)})
